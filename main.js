aa7680b21823cb4af7523e09e6001d7baa0e22e3
OfficialMaritza <101466695+OfficialMaritza@users.noreply.github.com>
2023-02-04 16:32:35 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+status = "";
+video = "";
+
+function preload(){
+
+  video = createVideo('video.mp4');
+  video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+
+function draw() {
+   image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = m15.objectDetector('cocossd', mdelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
